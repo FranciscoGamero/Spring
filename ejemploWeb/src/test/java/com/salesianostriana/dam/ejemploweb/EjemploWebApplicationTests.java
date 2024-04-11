426a3f11e9612aa95ae158e906f5f102dbb312c8
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2024-04-11 12:43:48 +0200

    Todo

---
+++ ejemploWeb/src/test/java/com/salesianostriana/dam/ejemploweb/EjemploWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
